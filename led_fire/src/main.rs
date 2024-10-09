42f77857c8fc4401c31b8231cf23b72068c5fef4
thehappycheese <2748120+thehappycheese@users.noreply.github.com>
2024-10-10 00:30:52 +0800

    working. just needs servo to behave nicely

--- led_fire/src/main.rs
+++ led_fire/src/main.rs
@@ -1,6 +1,5 @@
 use rppal::gpio::Gpio;
-use rppal::i2c::I2c;
-use std::{thread, time::Duration};
+use std::{sync::mpsc, thread, time::Duration};
 use std::fs::File;
 use std::io::BufReader;
 
@@ -12,13 +11,30 @@ use led::run_leds;
 mod pca9685;
 use pca9685::PCA9685;
 
-fn main(){
+enum MusicControl {
+    Play,
+}
 
-    let threads = vec![
-        thread::spawn(||run_leds()),
-        thread::spawn(||play_music()),
-        thread::spawn(||poll_gpio_pin()),
-        thread::spawn(||run_i2c()),
+fn main(){
+    let (tx, rx) = mpsc::channel();
+
+    let threads: Vec<std::thread::JoinHandle<()>> = vec![
+        thread::spawn(||{
+            run_leds();
+            ()
+        }),
+        thread::spawn(move || {
+            play_music(rx);
+            ()
+        }),
+        thread::spawn(move || {
+            poll_gpio_pin(tx);
+            ()
+        }),
+        thread::spawn(||{
+            run_i2c();
+            ()
+        }),
     ];
     for thread in threads {
         let _ = thread.join();
@@ -35,28 +51,37 @@ fn run_i2c(){
     }
 }
 
-fn play_music() {
+fn play_music(rx: mpsc::Receiver<MusicControl>) {
     // Get an output stream handle to the default physical sound device
-    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    // Load a sound from a file, using a path relative to Cargo.toml
-    let file = BufReader::new(File::open("../music/pied piper 3.mp3").unwrap());
-    // Decode that sound file into a source
-    let source:SamplesConverter<Decoder<BufReader<File>>, f32> = Decoder::new(file).unwrap().convert_samples();
-    let sink = Sink::try_new(&stream_handle).unwrap();
-    sink.append(source);
-    sink.sleep_until_end();
+    loop{
+        if let Ok(_) = rx.recv() {
+            let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+            // Load a sound from a file, using a path relative to Cargo.toml
+            let file = BufReader::new(File::open("../music/pied piper 3.mp3").unwrap());
+            // Decode that sound file into a source
+            let source:SamplesConverter<Decoder<BufReader<File>>, f32> = Decoder::new(file).unwrap().convert_samples();
+            let sink = Sink::try_new(&stream_handle).unwrap();
+            sink.append(source);
+            sink.pause();
+            sink.play();
+            sink.sleep_until_end();
+        }
+    }
 }
 
-fn poll_gpio_pin(){
+fn poll_gpio_pin(tx: mpsc::Sender<MusicControl>){
     let gpio = Gpio::new().expect("Failed to initialize GPIO");
     let pin_number = 17;
     let pin = gpio
         .get(pin_number)
         .expect("Failed to get GPIO pin")
-        .into_input();
+        .into_input_pulldown();
+
     loop {
-        let level = pin.read();
-        //println!("Motion detected: {:?}", level == rppal::gpio::Level::High);
+        if pin.is_high() {
+            println!("Button Pressed!");
+            tx.send(MusicControl::Play);
+        }
         thread::sleep(Duration::from_millis(200));
     }
 }
--- led_fire/src/pca9685.rs
+++ led_fire/src/pca9685.rs
@@ -1,78 +1,64 @@
 use std::{thread, time::Duration};
 use rppal::i2c::{I2c, Error as I2CError};
 
-
 /// Bit masks for MODE1 register settings.
-#[repr(u8)]
-enum M1 {
-    RESTART = 0b10000000,
-    EXTCLK  = 0b01000000,
-    AI      = 0b00100000,
-    SLEEP   = 0b00010000,
-    SUB1    = 0b00001000,
-    SUB2    = 0b00000100,
-    SUB3    = 0b00000010,
-    ALLCALL = 0b00000001,
-    NORMAL  = 0,
-}
+const MODE1_RESTART: u8 = 0b10000000;
+// const MODE1_EXTCLK : u8 = 0b01000000;
+// const MODE1_AI     : u8 = 0b00100000;
+const MODE1_SLEEP  : u8 = 0b00010000;
+// const MODE1_SUB1   : u8 = 0b00001000;
+// const MODE1_SUB2   : u8 = 0b00000100;
+// const MODE1_SUB3   : u8 = 0b00000010;
+// const MODE1_ALLCALL: u8 = 0b00000001;
+const MODE1_NORMAL : u8 = 0b00000000;
 
 /// Bit masks for MODE2 register settings.
-#[repr(u8)]
-enum M2 {
-    INVERT  = 0b00010000,
-    OCH     = 0b00001000,
-    OUTDRV  = 0b00000100,
-    OUTNE_H = 0b00000010,
-    OUTNE_L = 0b00000001,
-}
+// const MODE2_INVERT : u8 = 0b00010000;
+// const MODE2_OCH    : u8 = 0b00001000;
+const MODE2_OUTDRV : u8 = 0b00000100;
+// const MODE2_OUTNE_H: u8 = 0b00000010;
+// const MODE2_OUTNE_L: u8 = 0b00000001;
 
 /// Register addresses
-#[repr(u8)]
-#[allow(non)]
-enum REG {
-    MODE1      = 0x00,
-    MODE2      = 0x01,
-    PRE_SCALE  = 0xFE,
-    LED0_ON_L  = 0x06,
-    LED0_ON_H  = 0x07,
-    LED0_OFF_L = 0x08,
-    LED0_OFF_H = 0x09,
-}
+const REG_MODE1     : u8 = 0x00;
+const REG_MODE2     : u8 = 0x01;
+const REG_PRE_SCALE : u8 = 0xFE;
+const REG_LED0_ON_L : u8 = 0x06;
+// const REG_LED0_ON_H : u8 = 0x07;
+// const REG_LED0_OFF_L: u8 = 0x08;
+// const REG_LED0_OFF_H: u8 = 0x09;
 
-fn compute_prescale_value_from_hertz(frequency:f32) -> u8 {
-    let prescale_val:u8 = ((25_000_000.0 / (4096.0 * frequency)).round() - 1.0) as u8;
+fn compute_prescale_value_from_hertz(frequency: f32) -> u8 {
+    let prescale_val: u8 = ((25_000_000.0 / (4096.0 * frequency)).round() - 1.0) as u8;
     prescale_val
 }
 
-pub struct PCA9685{
-    device:I2c,
+pub struct PCA9685 {
+    device: I2c,
 }
 
 impl PCA9685 {
-
-    pub fn new(address:u16, bus:u8) -> Result<Self, I2CError>{
+    pub fn new(address: u16, bus: u8) -> Result<Self, I2CError> {
         let mut device = I2c::with_bus(bus)?;
         // Initializes the PCA9685 device with the specified PWM frequency.
        
         device.set_slave_address(address)?;
-        device.write(&[REG::MODE1 as u8, M1::SLEEP as u8])?;
-        device.write(&[REG::PRE_SCALE as u8, compute_prescale_value_from_hertz(50.0)])?;
-        device.write(&[REG::MODE1 as u8, M1::RESTART as u8])?;
+        device.write(&[REG_MODE1, MODE1_SLEEP])?;
+        device.write(&[REG_PRE_SCALE, compute_prescale_value_from_hertz(50.0)])?;
+        device.write(&[REG_MODE1, MODE1_RESTART])?;
 
         // wait for the device to restart
         thread::sleep(Duration::from_millis(5));
-        device.write(&[REG::MODE1 as u8, M1::NORMAL as u8])?; // Otherwise in low power mode by default
-        device.write(&[REG::MODE2 as u8, M2::OUTDRV as u8])?; // Output drive configuration
+        device.write(&[REG_MODE1, MODE1_NORMAL])?; // Otherwise in low power mode by default
+        device.write(&[REG_MODE2, MODE2_OUTDRV])?; // Output drive configuration
 
-        Ok(PCA9685 {
-            device,
-        })
+        Ok(PCA9685 { device })
     }
 
     /// Sets the PWM signal for a specific channel.
     /// This function is private.
     fn set_pwm(&mut self, channel: u8, on: u16, off: u16) -> Result<(), I2CError> {
-        let base = REG::LED0_ON_L as u8 + 4 * channel;
+        let base = REG_LED0_ON_L + 4 * channel;
         self.device.write(&[base, (on & 0xFF) as u8])?;
         self.device.write(&[base + 1, (on >> 8) as u8])?;
         self.device.write(&[base + 2, (off & 0xFF) as u8])?;
@@ -80,7 +66,7 @@ impl PCA9685 {
         Ok(())
     }
 
-    pub fn set_servo(&mut self, servo_number:u8, position:f32) -> Result<(), I2CError>{
+    pub fn set_servo(&mut self, servo_number: u8, position: f32) -> Result<(), I2CError> {
         assert!(servo_number < 16, "Servo number must be between 0 and 15");
         assert!(
             position >= 0.0 && position <= 1.0,
