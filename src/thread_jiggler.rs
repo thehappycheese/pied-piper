701cbeabde601f8ed04fbe09c6ada39224ebd860
thehappycheese <2748120+thehappycheese@users.noreply.github.com>
2024-10-13 18:52:55 +0800

    add some boops to the jiggler startup

--- src/thread_jiggler.rs
+++ src/thread_jiggler.rs
@@ -1,4 +1,4 @@
-use rodio::{OutputStream, Sink, Source};
+use rodio::{OutputStream, OutputStreamHandle, Sink, Source};
 use rodio::source::SineWave;
 use std::time::Duration;
 use std::thread;
@@ -7,6 +7,7 @@ use std::thread;
 pub fn keep_speaker_awake() {
 
     // Get the default output stream
+    println!("JIGGLER: WILL WAIT FOR AUDIO DEVICE");
     let x;
     loop{
         if let Ok(os) = OutputStream::try_default(){
@@ -20,7 +21,24 @@ pub fn keep_speaker_awake() {
     }
     let (_stream, stream_handle) = x;
 
+    println!("JIGGLER: GOT AUDIO DEVICE");
+
+    // Wait for bluetooth speaker to be ready
+    thread::sleep(Duration::from_secs(5));
+    
+    println!("JIGGLER: BOOP BOOP BOOP!");
+    // Startup Boop
+    boop(&stream_handle);
+    thread::sleep(Duration::from_secs_f32(0.1));
+    boop(&stream_handle);
+    thread::sleep(Duration::from_secs_f32(0.1));
+    boop(&stream_handle);
+    thread::sleep(Duration::from_secs_f32(0.1));
+
     loop {
+        
+        thread::sleep(Duration::from_secs(120));
+
         // Create a sink
         let sink = Sink::try_new(&stream_handle).unwrap();
 
@@ -39,6 +57,16 @@ pub fn keep_speaker_awake() {
         // Wait for the sink to finish
         sink.sleep_until_end();
 
-        thread::sleep(Duration::from_secs(120));
     }
+}
+
+
+/// Notify the user that the system has started up as intended
+fn boop(stream_handle:&OutputStreamHandle){
+    let sink = Sink::try_new(&stream_handle).unwrap();
+    let source = SineWave::new(300.0)
+        .take_duration(Duration::from_secs_f32(0.2))
+        .amplify(0.1);
+    sink.append(source);
+    sink.sleep_until_end();
 }
